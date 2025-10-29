4824674faaf96c9f085e0522c5b8b66a6af68fce
sofiabarrios <sofiacbarr@gmail.com>
2025-10-29 12:36:05 -0500

    first-commit

---
+++ src/test/java/org/example/Pattern/EmptyMethodTest.java
@@ -0,0 +1,14 @@
+package org.example.Pattern;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmptyMethodTest {
+
+    @Test
+    @DisplayName("Empty Method ")
+    void emptyMethodTest() {
+    }
+}
